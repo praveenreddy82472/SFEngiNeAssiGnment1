547cc0ddaa3d30f5509a243b3b84ced41192946e
PraveenReddy <tumatipraveenreddy23@gmail.com>
2023-07-28 14:38:28 -0500

    first commit

---
+++ week4/Assignment 1/Assignment1.py
@@ -0,0 +1,45 @@
+"""
+Program Title : Near Misses
+Name of the file : Assignment 1
+Name of the Programmer: Thumati Praveen Kumar
+Date : 7/15/2023
+Explanation of the Program : the Program searchs for  near miss with the value of n and K. the n value should be between 2 and 12 , and k value should be 
+                                greater than 10. if theses two conditions are statisfied then it will search for near misses. it works on the formula given 
+                                in the requirement to calculate the near misses
+"""
+
+
+# the n and k values are set to be 1
+n_value = 1
+k_value = 1
+
+def main1():
+    
+    while True:
+        # getting input from the user
+        n_value = int(input("Enter value for n such that 2 < n < 12: "))
+        
+        # the loop has been create to check the condition
+        if ((n_value <= 2) or (n_value >= 12)):
+            print("Invalid input! enter a number between 2 and 12")
+        else:
+                # getting k value from the user 
+                k_value= int(input("Enter upper limit k for x and y (k > 10): "))
+                if k_value >= 10:
+                    past_miss=None 
+                    for x in range(10, k_value+1): 
+                        for y in range(x,k_value+1): 
+                            pow_var = pow(x, n_value) + pow(y, n_value)
+                            z = int(pow(pow_var, 1/n_value))
+                            pow_of_z = pow(z, n_value)
+                            pow_z = pow(z+1, n_value)
+                            miss_value = min( pow_var - pow_of_z, pow_z - pow_var)
+                            miss_rel = miss_value/pow_var
+                            rel_mis = miss_rel
+                            print("\nx = {} y = {}  z = {}  Miss = {}  Relative Miss = {}%".format(x, y, z, miss_value, round(rel_mis*100,2)))
+                    break
+                else:
+                    print("Invalid input!!!! enter a number greater than 10")
+                    
+                    
+main1()
---
+++ week4/Readme.txt
@@ -0,0 +1,13 @@
+Software Engineering: Assignment 1
+Fermat’s Last Theorem Near Misses
+
+Step 1: Open cmd in the Assignment Location
+
+Step 2: Type "python Assignment1.py" in the cmd
+
+Step 3: Enter the value of n
+
+Step 4: Enter the value of k
+
+Step 5: Near miss output will be displayed
+
